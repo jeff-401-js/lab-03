48b0f4dba20288e3c969d24e36c95a3b49a74de4
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-23 09:28:18 -0700

    test refactor for promises after demo

--- __tests__/lib/reader-promises.test.js
+++ __tests__/lib/reader-promises.test.js
@@ -4,16 +4,11 @@ jest.mock('fs');
 
 const readerPromises = require('../../lib/reader-promises.js');
 
-describe('File Reader promises Module', () => {
+describe('File Reader promises Module fails', () => {
 
   it('when given a bad file, returns an error', () => {
     let files = ['bad.txt'];
-    // In jest, throwing errors obviously kills the app, so if you're
-    // going to throw one in a test, have the expect execute your code as a
-    // function so that you can trap it.
-    // reader(files, (err,data) => {
-    //   expect(err).toBeDefined();
-    //   done();
+
     return readerPromises(files)
       .then()
       .catch(err => expect(err).toBeDefined());
@@ -21,14 +16,13 @@ describe('File Reader promises Module', () => {
 
 
 
-  it('reads 3 files', done => {
-    let files = ['file1.txt', 'file2.txt', 'file2.txt'];
-    reader(files, (err,data) => {
-      expect(err).toBeNull();
-      expect(data instanceof Array ).toBeTruthy();
-      expect(data.length ).toBe(3);
-      done();
-    });
-  });
+  it('reads 3 files', () => {
+    let files = ['File Contents', 'File Contents', 'File Contents'];
 
+    return readerPromises(files)
+      .then(result => {
+        expect(result).toEqual(files);
+      });
+  });
 });
+
