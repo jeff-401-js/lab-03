dc5e9917f68da052c314168c8f866935ae09970e
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-23 19:53:41 -0700

    test two

--- __tests__/lib/reader-async.test.js
+++ __tests__/lib/reader-async.test.js
@@ -11,17 +11,18 @@ describe('File Reader promises Module fails', () => {
     try{
       let result = await readerAsync(false);
       expect(result).toBeUndefined();
-    }catch(err){}
+    }catch(err){
+
+    }
   });
 
-
-
-  it('reads 3 files', () => {
+  it('reads 3 files', async () => {
     let files = ['File Contents', 'File Contents', 'File Contents'];
+    try{
+      let result = await readerAsync(files);
+      expect(result).toEqual(files);
+    }catch(err){
 
-    return readerPromises(files)
-      .then(result => {
-        expect(result).toEqual(files);
-      });
+    }
   });
 });
