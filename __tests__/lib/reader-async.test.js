e6bd2107f3ee54d6c396384c3533d2cb86552ba4
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-23 19:43:33 -0700

    testing

--- __tests__/lib/reader-async.test.js
+++ __tests__/lib/reader-async.test.js
@@ -1,1 +1,27 @@
 'use strict';
+
+jest.mock('fs');
+
+const readerPromises = require('../../lib/reader-promises.js');
+
+describe('File Reader promises Module fails', () => {
+
+  it('when given a bad file, returns an error', () => {
+    let files = ['bad.txt'];
+
+    return readerPromises(files)
+      .then()
+      .catch(err => expect(err).toBeDefined());
+  });
+
+
+
+  it('reads 3 files', () => {
+    let files = ['File Contents', 'File Contents', 'File Contents'];
+
+    return readerPromises(files)
+      .then(result => {
+        expect(result).toEqual(files);
+      });
+  });
+});
